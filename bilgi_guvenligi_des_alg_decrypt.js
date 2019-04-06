eeee3612d421474c5f7dd99c238cc21778ba3459
eddysnjack <newgardenknight@mail.ru>
2019-04-06 18:52:55 +0300

    111111111111

---
+++ bilgi_guvenligi_des_alg_decrypt.js
@@ -0,0 +1,20 @@
+//DECRYPTION BUTTON CHAR
+function btn_decrypt_CHAR_event() {
+
+}
+
+
+
+
+//DECRYPTION BUTTON INTEGER
+function btn_decrypt_INT_event() {
+
+}
+
+
+
+
+//DECRYPTION BUTTON HEX
+function btn_decrypt_HEX_event() {
+
+}
